6cc0ac5e72d119372b60d41bb896c63c896e213f
thinhnsd <thinhnsd@gmail.com>
2024-03-12 22:55:51 +0700

    ok

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const mysql = require('mysql2');
 const Pusher = require('pusher');
 
+const http = require('http');
+const socketIo = require('socket.io');
+
 require('dotenv').config();
 
 const connection = mysql.createConnection({
@@ -14,6 +17,16 @@ const connection = mysql.createConnection({
   waitForConnections: true,
   queueLimit: 0
 });
+
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err.stack);
+    return;
+  }
+  console.log('Connected to MySQL as id', connection.config.database);
+});
+
 
 const pusher = new Pusher({
   appId: "1770077",
@@ -27,38 +40,54 @@ const pusher = new Pusher({
 
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+process.setMaxListeners(15);
 
 app.use(cors({
-  origin : ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200']
+  origin : ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200', 'http://localhost:8000']
 }))
 
 app.use(express.json())
 
+
+app.get('/api/get-messages', (req, res) => {
+  const sql = "SELECT * FROM user_message"; 
+  connection.query(sql, function (err, result) {
+    if (err) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.json(result);
+  });
+});
+
 app.post('/api/messages', async (req, res) => {
   const { username, message } = req.body;
-
-    // // Lưu dữ liệu vào bảng user_message trong cơ sở dữ liệu hn_2024
-    // connection.query(
-    //   'INSERT INTO user_message (username, message) VALUES (?, ?)',
-    //   [username, message],
-    //   (error, results, fields) => {
-    //     if (error) {
-    //       console.error('Error inserting into user_message:', error.stack);
-    //       return res.status(500).json({ error: 'Error inserting into user_message' });
-    //     }
-  
-    //     console.log('Inserted into user_message table:', results);
-    //   }
-    // );
 
   await pusher.trigger("chat", "message", {
     username,
     message,
   });
 
-  res.json([]);
+  io.emit('message', {
+    username,
+    message,
+  });
+
+  const sql = "INSERT INTO user_message (username, message) VALUES (?, ?)";
+  connection.query(sql, [username, message], function (err, result) {
+    if (err) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    console.log("1 record inserted");
+    res.json([]);
+  });
 });
 
-app.listen(8080)
+
+
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
 
 
